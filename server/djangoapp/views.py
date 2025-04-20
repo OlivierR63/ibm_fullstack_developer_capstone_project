73048f3595290a01bca745dc112866a4a9c85d7f
OlivierR63 <olivier.rochat@gmail.com>
2025-04-20 13:10:56 -0400

    Bug fix in djangoapp/views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -96,8 +96,8 @@ def get_dealer_reviews(request, dealer_id):
         reviews = get_request(endpoint)
         
         for review_detail in reviews: 
-            response = analyze_review_sentiments(review['review'])
-            reviews_detail['sentiment'] = response['sentiment']
+            response = analyze_review_sentiments(review_detail['review'])
+            review_detail['sentiment'] = response['sentiment']
 
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
