8cf6f8bc91eb529c62b99c9fe6f8b4f7f272e7fe
OlivierR63 <olivier.rochat@gmail.com>
2025-04-22 11:49:15 -0400

    Update the Dealers.jsx file: Replaced the dropdown list in the 'state' column with a searchable textbox

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -6,38 +6,40 @@ import review_icon from "../assets/reviewicon.png"
 
 const Dealers = () => {
     const [dealersList, setDealersList] = useState([]);
-    // let [state, setState] = useState("")
-    let [states, setStates] = useState([])
+    const [states, setStates] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [originalDealers, setOriginalDealers] = useState([]);
 
-    // let root_url = window.location.origin
-    let dealer_url ="/djangoapp/get_dealers";
-    
-    let dealer_url_by_state = "/djangoapp/get_dealers/";
-    
-    const filterDealers = async (state) => {
-        dealer_url_by_state = dealer_url_by_state +state;
-        const res = await fetch(dealer_url_by_state, {
-        method: "GET"
-        });
-        const retobj = await res.json();
-        if(retobj.status === 200) {
-        let state_dealers = Array.from(retobj.dealers)
-        setDealersList(state_dealers)
+    const dealer_url ="/djangoapp/get_dealers";
+
+    // Function for handling changes in search input
+    const handleInputChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        const filtered = originalDealers.filter(dealer =>
+            dealer.state.toLowerCase().includes(query.toLowerCase())
+          );
+        setDealersList(filtered);
+    };
+
+    // Function to manage the loss of focus of the search input
+    const handleLostFocus = () =>{
+        if (!searchQuery){
+            setDealersList(originalDealers);
         }
-    }
+    };
 
+    // Function to retrieve dealers from the API
     const get_dealers = useCallback(async ()=>{
         const res = await fetch(dealer_url, {method: "GET"});
         const retobj = await res.json();
         if(retobj.status === 200) {
-            let all_dealers = Array.from(retobj.dealers);
-            let states = [];
-            all_dealers.forEach((dealer)=>{
-                states.push(dealer.state)
-            });
+            const all_dealers = Array.from(retobj.dealers);
+            const states = Array.from(new Set(all_dealers.map(dealer=>dealer.state)));
 
-            setStates(Array.from(new Set(states)))
+            setStates(states);
             setDealersList(all_dealers)
+            setOriginalDealers(all_dealers);
         }
     }, [dealer_url]);
     
@@ -45,47 +47,56 @@ const Dealers = () => {
         get_dealers();
     },[get_dealers]);  
 
-    let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
+    const isLoggedIn = sessionStorage.getItem("username") != null;
+
     return(
     <div>
         <Header/>
 
         <table className='table'>
-        <tr>
-        <th>ID</th>
-        <th>Dealer Name</th>
-        <th>City</th>
-        <th>Address</th>
-        <th>Zip</th>
-        <th>
-        <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-        <option value="" selected disabled hidden>State</option>
-        <option value="All">All States</option>
-        {states.map(state => (
-            <option value={state}>{state}</option>
-        ))}
-        </select>        
-
-        </th>
-        {isLoggedIn ? (
-            <th>Review Dealer</th>
-            ):<></>
-        }
-        </tr>
-        {dealersList.map(dealer => (
-            <tr>
-            <td>{dealer['id']}</td>
-            <td><a href={'/dealer/'+dealer['id']}>{dealer['full_name']}</a></td>
-            <td>{dealer['city']}</td>
-            <td>{dealer['address']}</td>
-            <td>{dealer['zip']}</td>
-            <td>{dealer['state']}</td>
-            {isLoggedIn ? (
-                <td><a href={`/postreview/${dealer['id']}`}><img src={review_icon} className="review_icon" alt="Post Review"/></a></td>
-            ):<></>
-            }
-            </tr>
-        ))}
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Dealer Name</th>
+                    <th>City</th>
+                    <th>Address</th>
+                    <th>Zip</th>
+                    <th>
+                        <input type="text"
+                                placeholder="Search states..."
+                                onChange={handleInputChange}
+                                onBlur={handleLostFocus}
+                                value={searchQuery}
+                                />       
+                    </th>
+                    {
+                        isLoggedIn && (
+                            <th>Review Dealer</th>
+                        )
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {dealersList.map(dealer => (
+                    <tr key={dealer.id}>
+                        <td>{dealer.id}</td>
+                        <td><a href={`/dealer/${dealer.id}`}>{dealer.full_name}</a></td>
+                        <td>{dealer.city}</td>
+                        <td>{dealer.address}</td>
+                        <td>{dealer.zip}</td>
+                        <td>{dealer.state}</td>
+                        {
+                            isLoggedIn && (
+                                <td>
+                                    <a href={`/postreview/${dealer.id}`}>
+                                        <img src={review_icon} className="review_icon" alt="Post Review"/>
+                                    </a>
+                                </td>
+                            )
+                        }
+                    </tr>
+                ))}
+            </tbody>
         </table>;
     </div>
     )
