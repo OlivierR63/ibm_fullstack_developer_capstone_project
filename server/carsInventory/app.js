dae5c11b910d556e867947741a852c1cf3c0d16d
OlivierR63 <olivier.rochat@gmail.com>
2025-04-23 17:16:47 -0400

    Append carsInventory/Dockerfile and carsInventory/docker-compose.yml

--- server/carsInventory/app.js
+++ server/carsInventory/app.js
@@ -1,12 +1,13 @@
-const express = require ('express');
+
+const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const cors = require('');
+const cors = require('cors');
 
-const app = require(express());
+const app = express();
 const port = 3050;
 
-app.use(cors);
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 const carsData = JSON.parse(fs.readFileSync('cars_records.json'));
@@ -32,10 +33,90 @@ app.get('/', async (req, res) => {
 
 app.get('/cars/:id', async (req, res) => {
     try{
-        const document = await Cars.find(dealers_id : req.params.id);
-        req.json(document);
+        const document = await Cars.find({dealers_id: req.params.id});
+        res.json(document);
     } 
     catch(error){
-        res.status(500).json({error: 'Error fetching reviews'});
+        res.status(500).json({error: 'Error fetching cars'});
     };
+});
+
+app.get('/carsbymake/:id/:make', async (req, res) => {
+    try{
+        const document = await Cars.find({dealers_id: req.params.id, make: req.params.make});
+        res.json(document);
+    } 
+    catch(error){
+        console.error('Error fetching cars:', error); 
+        res.status(500).json({error: 'Error fetching cars'});
+    };
+});
+
+app.get('/carsbymodel/:id/:model', async (req, res) => {
+    try{
+        const document = await Cars.find({dealers_id: req.params.id, model: req.params.model});
+        res.json(document);
+    } 
+    catch(error){
+        console.error('Error fetching cars:', error); 
+        res.status(500).json({error: 'Error fetching cars'});
+    };
+});
+
+app.get('/carsbymaxmileage/:id/:mileage', async (req, res) => {
+    try {
+        let mileage = parseInt(req.params.mileage)
+        let condition = {}
+        if(mileage === 50000) {
+          condition = { $lte : mileage}
+        } else if (mileage === 100000){
+          condition = { $lte : mileage, $gt : 50000}
+        } else if (mileage === 150000){
+          condition = { $lte : mileage, $gt : 100000}
+        } else if (mileage === 200000){
+          condition = { $lte : mileage, $gt : 150000}
+        } else {
+          condition = { $gt : 200000}
+        }
+        const documents = await Cars.find({ dealers_id: req.params.id, mileage : condition });
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+      }
+    });
+
+app.get('/carsbyprice/:id/:price', async (req, res) => {
+    try {
+        let price = parseInt(req.params.price)
+        let condition = {}
+        if(price === 20000) {
+            condition = { $lte : price}
+        } else if (price=== 40000){
+            console.log("\n \n \n "+ price)  
+            condition = { $lte : price, $gt : 20000}
+        } else if (price === 60000){
+            condition = { $lte : price, $gt : 40000}
+        } else if (price === 80000){
+            condition = { $lte : price, $gt : 60000}
+        } else {
+            condition = { $gt : 80000}
+        }
+        const documents = await Cars.find({ dealers_id: req.params.id, price : condition });
+        res.json(documents);
+        } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
+        }
+});
+
+app.get('/carsbyyear/:id/:year', async (req, res) => {
+    try {
+    const documents = await Cars.find({ dealers_id: req.params.id, year : { $gte :req.params.year }});
+        res.json(documents);
+    } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealers by ID' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
