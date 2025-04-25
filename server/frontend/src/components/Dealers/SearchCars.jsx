1ade025186ba8e1d3f6c9aea4524a1309fe33a05
OlivierR63 <olivier.rochat@gmail.com>
2025-04-25 14:35:16 -0400

    Complete file SearchCars.jsx

--- server/frontend/src/components/Dealers/SearchCars.jsx
+++ server/frontend/src/components/Dealers/SearchCars.jsx
@@ -6,7 +6,7 @@ import Header from '../Header/Header';
 const SearchCars = () => {
     const [cars, setCars] = useState([]);
     const [makes, setMakes] = useState([]);
-    const [model, setModel] = useState([]);
+    const [model, setModels] = useState([]);
     const [dealer, setDealer] = useState({"full_name":""});
     const [message, setMessage] = useState("Loading Cars ...");
 
@@ -15,5 +15,43 @@ const SearchCars = () => {
 
     let dealer_url = '/djangoapp/get_inventory/${id}';
     let fetch_url = '/djangoapp/dealer/${id}';
+
+    const fetchDealer = async () => {
+        const res = await fetch(fetch_url, {
+            method: "GET"
+        });
+
+        const retobj = await res.json;
+        if (retobj.status === 200) {
+            let dealer = retobj.dealer;
+            setDealer({"full_name": dealer[0].full_name});
+        }
+    };
+
+
+    const populateMakesAndModels = (cars)=>{
+        let tmpmakes = [];
+        let tmpmodels = [];
+        cars.forEach((car)=>{
+            tmpmakes.push(car.make);
+            tmpmodels.push(car.model);
+        });
+        setMakes(Array.from(new Set(tmpmakes)));
+        setModels(Array.from(new Set(tmpmodels)));
+    };
+
+
+    const fetchCars = async () => {
+        const res = await fetch(dealer_url, {
+            method: "GET"
+        });
+        const retobj = await res.json();
+
+        if (retobj.status === 200){
+            let cars = Array.from(retobj.cars);
+            setCars(cars);
+            populateMakesAndModels(cars);
+        };
+    };
     
 }
