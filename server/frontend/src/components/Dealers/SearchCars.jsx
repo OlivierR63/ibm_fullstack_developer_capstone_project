ad9c9c0564d9caec0409459fe345dfe7418fc20c
OlivierR63 <olivier.rochat@gmail.com>
2025-04-25 16:21:24 -0400

    Carry on completion of file SearchCars.jsx

--- server/frontend/src/components/Dealers/SearchCars.jsx
+++ server/frontend/src/components/Dealers/SearchCars.jsx
@@ -53,5 +53,193 @@ const SearchCars = () => {
             populateMakesAndModels(cars);
         };
     };
+
+
+    const setCarsMatchingCriteria = async(matching_cars) => {
+        let cars = Array.from(matching_cars);
+        console.log(`Numbers of matching cars : ${matching_cars}`);
+
+        let makeIdx = document.getElementById('make').selectedIndex;
+        let modelIdx = document.getElementById('model').selectedIndex;
+        let yearIdx = document.getElementById('year').selectedIndex;
+        let mileageIdx = document.getElementById('mileage').selectedIndex;
+        let priceIdx = document.getElementById('price').selectedIndex;
+
+        if (makeIdx !== 0) {
+            let currmake = document.getElementById('make').value;
+            cars = cars.filter(car => car.make === currmake);
+        }
+
+        if (modelIdx !== 0) {
+            let currmodel = document.getElementById('model').value;
+            cars = cars.filter(car => car.model === currmodel);
+            if(cars.length !== 0){
+                document.getELementById('make').value = cars[0].make;
+            }
+        }
+
+        if (yearIdx !== 0){
+            let curryear = document.getElementById('year').value;
+            cars = cars.filter(car => car.year === curryear);
+
+            if (cars.length !== 0){
+                document.getELementById('make').value =cars[0].make;
+            }
+        }
+
+        if (mileageIdx !== 0){
+            let currmileage = parseInt(document.getElementById('mileage').value);
+
+            if(currmileage === 50000){
+                cars = cars.filter(car => car.mileage <= mileage)
+            } else if (currmileage === 100000){
+                cars = cars.filter(car => car.mileage >50000 && car.mileage <= currmileage);
+            } else if (currmileage === 150000){
+                cars = cars.filter(car => car.mileage >100000 && car.mileage <= currmileage);
+            } else if (currmileage === 200000){
+                cars = cars.filter(car => car.mileage >150000 && car.mileage <= currmileage);
+            } else {
+                cars = cars.filter(car => car.mileage > 200000);
+            }
+        }
+
+        if (priceIdx !== 0) {
+            let currprice = parseInt(document.getElementById('price').value);
+            if(currprice === 20000) {
+              cars = cars.filter(car => car.price <= currprice);
+            } else if (currprice === 40000){
+              cars = cars.filter(car => car.price <= currprice && car.price > 20000);
+            } else if (currprice === 60000){
+              cars = cars.filter(car => car.price <= currprice && car.price > 40000);
+            } else if (currprice === 80000){
+              cars = cars.filter(car => car.price <= currprice && car.price > 60000);
+            } else {
+              cars = cars.filter(car => car.price > 80000);
+            }
+        }
+
+        if (cars.length === 0){
+            setMessage("No cars found matching criteria");
+        }
+
+        setCars(cars);
+    };
+
+
+    let SearchCarsByMake = async () => {
+        let make = document.getElementById('make').value;
+        dealer_url = dealer_url + "?make=" + make;
+
+        const res = await fetch(dealer_url, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        const retobj = await res.json();
+        
+        if(retobj.status === 200){
+            setCarsMatchingCriteria(retobj.cars);
+        }
+    };
+
+
+    let SearchCarsByModel = async () => {
+        let model = document.getElementById('model').value;
+        dealer_url = dealer_url + "?model=" + model;
+
+        const res = await fetch(dealer_url, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        const retobj = await res.json();
+        
+        if(retobj.status === 200){
+            setCarsMatchingCriteria(retobj.cars);
+        }
+    };
+
+
+    let SearchCarsByYear = async () => {
+        let year = document.getElementById('year').value;
+        dealer_url = dealer_url + "?year=" + year;
+
+        const res = await fetch(dealer_url, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        const retobj = await res.json();
+        
+        if(retobj.status === 200){
+            setCarsMatchingCriteria(retobj.cars);
+        }
+    };
+
+
+    let SearchCarsByMileage = async () => {
+        let mileage = document.getElementById('mileage').value;
+        dealer_url = dealer_url + "?mileage=" + mileage;
+
+        const res = await fetch(dealer_url, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        const retobj = await res.json();
+        
+        if(retobj.status === 200){
+            setCarsMatchingCriteria(retobj.cars);
+        }
+    };
+
+
+    let SearchCarsByPrice = async () => {
+        let price = document.getElementById('price').value;
+        dealer_url = dealer_url + "?price=" + price;
+
+        const res = await fetch(dealer_url, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        });
+
+        const retobj = await res.json();
+        
+        if(retobj.status === 200){
+            setCarsMatchingCriteria(retobj.cars);
+        }
+    };
+
+    const reset = () =>{
+        const selectElements = document.querySelectorAll('select');
+
+        selectElements.forEach(select => {
+            select.selectedIndex = 0;
+        });
+
+        fetchCars();
+    }
+
+
+    useEffect(() => {
+        fetchCars();
+        fetchDealer();
+    }, []);
+
+    return(
+        <div>
+            
+        </div>
+    );
     
-}
+};
